86329b4383f48381c4108c2ec5426ac4cfc06be7
0xpradish <pradish2125@gmail.com>
2022-11-17 21:48:42 +0530

    new font

---
+++ edit.js
@@ -0,0 +1,15 @@
+let name='hello';
+// console.log(name);
+
+var a='world';
+console.log(name+' '+ a);
+
+var arr =[12,2,3,4,5,6];
+
+
+
+
+console.log(arr.length);
+arr.pop();
+console.log(arr);
+console.log(arr.length);
